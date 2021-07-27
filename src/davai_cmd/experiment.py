4aa47f4ad75b652d372abb63ce0fe63bbdbc9e9a
MARY Alexandre <alexandre.mary@meteo.fr>
2021-07-27 15:01:12 +0000

    cosmetic changes

--- src/davai_cmd/experiment.py
+++ src/davai_cmd/experiment.py
@@ -140,10 +140,10 @@ class AnXP(object):
 
     def _set_runs(self):
         """Set run-wrapping scripts."""
-        for r in ('runjob.py', 'RUN_XP.sh', 'setup_ciboulai.sh', 'packbuild.sh'):
+        for r in ('runjob.py', 'RUN_XP.sh', '1.setup_ciboulai.sh', '2.packbuild.sh'):
             self._set(os.path.join(this_repo_tests, 'src', 'runs', r), r)
         self._set(os.path.join(this_repo_tests, 'src', 'runs', '{}_tests.sh'.format(self.usecase)),
-                  'tests.sh')
+                  '3.tests.sh')
 
     def _link_packages(self):
         """Link necessary packages in XP."""
