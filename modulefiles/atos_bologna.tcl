38c41d1e9a74cf43ba2c35f8db019af6e39db77d
Alexandre Mary <alexandre.mary@meteo.fr>
2023-01-12 17:45:42 +0000

    forgotten 3 in shebang

---
+++ modulefiles/atos_bologna.tcl
@@ -0,0 +1,31 @@
+#%Module1.0
+
+# path to DAVAI-env on belenos
+set provider_user ~rm9
+set modroot $provider_user/public/DAVAI-env
+
+module-whatis "Environment module for DAVAI-env"
+module-whatis "Cf. https://github.com/ACCORD-NWP/DAVAI-env"
+module-whatis "Local installation: $modroot"
+
+# pre-requisites
+if { [module-info mode load] || [module-info mode switch2] } {
+  puts stderr "Check davai requirements: git, python..."
+}
+prereq git
+prereq python
+if { [module-info mode load] || [module-info mode switch2] } {
+  puts stderr "...OK"
+}
+
+set modulename [module-info name]
+set fields [split $modulename "/"]
+set pkg_name [lindex $fields 0]
+set pkg_vers [lindex $fields end]
+
+prepend-path PATH $modroot/$pkg_vers/bin
+prepend-path PYTHONPATH $modroot/$pkg_vers/src
+
+if { [module-info mode load] || [module-info mode switch2] } {
+  puts stderr "Environment for DAVAI-env commands loaded"
+}
